1570d736ef1a81d053b99b9ae087d8a3204d248d
Tomas Marmay <tomasmarmay21@gmail.com>
2023-10-28 10:26:21 -0300

    segunda parte terminada

--- practico/practico7/practico7.mongodb.js
+++ practico/practico7/practico7.mongodb.js
@@ -263,3 +263,62 @@ db.comments
 	}
 	)	
 
+// PARTE 2
+
+/* 
+ejercicio 10
+Listar el id del restaurante (restaurant_id) y las calificaciones de los restaurantes 
+donde al menos una de sus calificaciones haya sido realizada entre 2014 y 2015 inclusive, 
+y que tenga una puntuación (score) mayor a 70 y menor o igual a 90.
+*/
+use("restaurantdb")
+db.restaurants.aggregate([
+	{
+	  $match: {
+		"grades": {
+		  $elemMatch: {
+			"date": {
+			  $gte: ISODate("2014-01-01T00:00:00.000Z"),
+			  $lt: ISODate("2016-01-01T00:00:00.000Z")
+			},
+			"score": {
+			  $gt: 70,
+			  $lte: 90
+			}
+		  }
+		}
+	  }
+	}
+  ])
+  
+/* 
+ejercicio11
+Agregar dos nuevas calificaciones al restaurante cuyo id es "50018608".
+A continuación se especifican las calificaciones a agregar en una sola consulta.  
+*/
+use("restaurantdb")
+db.restaurants.find({restaurant_id : "50018608"})
+
+use("restaurantdb")
+db.restaurants.updateOne(
+	{ restaurant_id: "50018608" },
+	{
+	  $push: {
+		grades: {
+		  $each: [
+			{
+				"date" : ISODate("2019-10-10T00:00:00Z"),
+				"grade" : "A",
+				"score" : 18
+			},
+			{
+				"date" : ISODate("2020-02-25T00:00:00Z"),
+				"grade" : "A",
+				"score" : 21
+			}
+		  ]
+		}
+	  }
+	}
+  )
+  
